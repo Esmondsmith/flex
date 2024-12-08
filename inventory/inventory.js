7d69966543098baf308c08ed1b9f2fc0ea3d35e0
Esmond Agharovbokhan <133409203+Esmondsmith@users.noreply.github.com>
2024-12-08 09:03:57 +0100

    updated

--- inventory/inventory.js
+++ inventory/inventory.js
@@ -1,76 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const homeLink = document.getElementById('homepage');
-    const entriesDefinitionLink = document.getElementById('entriesDef');
-    const inventoryParaLink = document.getElementById('inventoryPar');
-    const stockMagt = document.getElementById('stockItemMgt');
-    const stockTake = document.getElementById('stockTake');
-    const barcode = document.getElementById('barcode');
-    const openingStockBal = document.getElementById('openingStockTags');
-
-
-    const container = document.querySelector('.container');
-    const defaultPage = document.querySelector('.defaultPage');
-  
-    function loadPage(url) {
-      if (defaultPage) {
-        defaultPage.style.display = 'none'; 
-      }
-      fetch(url)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.text();
-        })
-        .then((html) => {
-          // Load the content into the container
-          container.innerHTML = html;
-        })
-        .catch((error) => {
-          console.error('Error loading the page:', error);
-          container.innerHTML = '<p>Error loading page. Please try again later.</p>';
-        })
-    }
-  
-    // To handle inventory settings
-    entriesDefinitionLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      loadPage('entriesDef.html');
-    });
-    inventoryParaLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      loadPage('inventPara.html');
-    });
-
-    //To handle inventory stocks
-    stockMagt.addEventListener('click', (event) => {
-      event.preventDefault();
-      loadPage('stockItemMgt.html');
-    });
-    stockTake.addEventListener('click', (event) => {
-      event.preventDefault();
-      loadPage('stockTake.html');
-    });
-    barcode.addEventListener('click', (event) => {
-      event.preventDefault();
-      loadPage('barcode.html');
-    });
-    openingStockBal.addEventListener('click', (event) => {
-      event.preventDefault();
-      loadPage('openingStocks.html');
-    });
-
-    // Handle the "Home" link click
-    homeLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      // Hide loaded content
-      container.innerHTML = '<h2>INVENTORY MODULE</h2> <p>Use sidebar to navigate through Inventory Module</p>';
-      // Show the default content
-      if (defaultPage) {
-        defaultPage.style.display = 'block';
-      }
-    });
-});
 
 function toggleCollapseStockItems() {
   var collapseElement = document.getElementById('voucherDropId');
@@ -81,5 +8,31 @@ function toggleCollapseStockItems() {
     stockBtnText.innerHTML = "Expand <i class='fa-solid fa-circle-chevron-down'></i>"
   }
   // collapseElement.classList.toggle('show');
+}
+
+// FOR STOCK ITEMS MANAGEMENT EXPAND  BUTTONS
+function toggleExpandStockItems() {
+  let collapseElement = document.getElementById('closeUpDownId');
+  let stockBtnText = document.getElementById('stockBtn');
+  if(collapseElement.classList.toggle('show')){
+    stockBtnText.innerHTML = "Close <i class='fa-solid fa-circle-chevron-up'></i>"
+  } else {
+    stockBtnText.innerHTML = "Expand <i class='fa-solid fa-circle-chevron-down'></i>"
+  }
+  // collapseElement.classList.toggle('show');
+}
+
+
+function showPage(pageId) {
+  const pageOneDiv = document.getElementById('page1');
+  const pageTwoDiv = document.getElementById('page2');
+  const pageThreeDiv = document.getElementById('page3');
+
+    if (pageOneDiv) pageOneDiv.style.display = "none";
+    if (pageTwoDiv) pageTwoDiv.style.display = "none";
+    if (pageThreeDiv) pageThreeDiv.style.display = "none";
+  
+    const targetPage = document.getElementById(pageId);
+    if (targetPage) targetPage.style.display = "block";
 }
 
